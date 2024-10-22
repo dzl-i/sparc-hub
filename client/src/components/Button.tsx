7c5d1a20d429fa8c7cad3bafbab22df167216ffe
Sebastien Singarayar <z5444568@ad.unsw.edu.au>
2024-10-22 20:36:02 +1100

    feat: add ripple onclick animation to button

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,10 +1,31 @@
-import { ReactNode } from "react";
+"use client";
+
+import React, { ReactNode } from "react";
 
 function Button({ children }: { children: ReactNode }) {
+  function createRipple(event: React.MouseEvent) {
+    const button = event.currentTarget as HTMLElement;
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
+    circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
+    circle.classList.add("ripple");
+    const ripple = button.getElementsByClassName("ripple")[0];
+    if (ripple) {
+      ripple.remove();
+    }
+    button.appendChild(circle);
+  }
   return(
     <>
       <div>
-        <button type="button" className="px-10 py-2 text-lg font-semibold font-spartan text-[hsl(50,21%,95%)] bg-[hsl(30,9%,17%)] hover:bg-[hsl(0,0%,0%)] duration-200 rounded-3xl outline-0 border-0">
+        <button
+          type="button"
+          className="relative overflow-hidden px-10 py-2 text-lg font-semibold font-spartan text-[hsl(50,21%,95%)] bg-[hsl(30,9%,17%)] hover:bg-[hsl(0,0%,0%)] duration-200 rounded-3xl outline-0 border-0"
+          onClick={createRipple}
+        >
           {children}
         </button>
       </div>
