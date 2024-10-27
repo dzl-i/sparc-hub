ff91465361933aeac8baaf9324bc62c7dcaeffbc
Sebastien Singarayar <z5444568@ad.unsw.edu.au>
2024-10-27 15:42:13 +1100

    fix: change button to div, change unaligned lalazar font to spartan

--- client/src/components/Chip.tsx
+++ client/src/components/Chip.tsx
@@ -5,33 +5,27 @@ export type ChipProps = {
   variant: "default" | "landingPage" | "top3";
 }
 
-function Chip( { children, variant="default"}: ChipProps) {
-  let styles = "px-4 py-1 text-black outline-0";
+function Chip( { children, variant="default" }: ChipProps ) {
+  let styles = "px-4 py-1 text-black text-center font-spartan w-full outline-0";
   switch(variant) {
     case "default":
-      styles += "text-sm font-spartan bg-transparent rounded-3xl border border-black"
+      styles += "text-sm bg-transparent rounded-3xl border border-black"
       break;
     case "landingPage":
-      styles += "text-sm font-lalazar bg-white rounded-3xl border-2 border-[darkGreen]"
+      styles += "text-sm font-semibold bg-white rounded-3xl border-2 border-[darkGreen]"
       break;
     case "top3":
-      styles += "font-lalazar text-lg bg-white rounded-xl border border-black"
+      styles += "text-lg font-semibold bg-white rounded-xl border border-black"
       break;
   }
 
   return (
     <>
-      <div>
-        <button
-          type="button"
-          className={styles}
-          disabled
-        >
-          {children}
-        </button>
+      <div className={styles}>
+        {children}
       </div>
     </>
-  );
+  )
 }
 
 export default Chip;
