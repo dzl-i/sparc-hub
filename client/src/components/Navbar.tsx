0cf374304df7062af831e704c311dd60db854e00
Sebastien Singarayar <z5444568@ad.unsw.edu.au>
2024-10-30 20:18:07 +1100

    style: use semantic html tags

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,23 +2,23 @@ import { Handshake, CircleUserRound, LogIn } from "lucide-react";
 
 function Navbar() {
   return (
-    <>
-      <div className="flex bg-slate-600">
-        <div className="flex h-screen justify-between flex-col bg-white py-4 w-20">
-          <div className="flex flex-col items-center gap-5">
-            <div>
-              <img className="h-10 hover:scale-105 transition-transform cursor-pointer" src="/assets/tempIcon.svg"></img>
-            </div>
-            <hr className="w-9/12 size-0.5 bg-gray-200"/>
-            <Handshake className="hover:scale-105 transition-transform cursor-pointer"></Handshake>
-          </div>
-          <div className="flex flex-col items-center gap-8">
-            <CircleUserRound className="hover:scale-105 transition-transform cursor-pointer"></CircleUserRound>
-            <LogIn className="hover:scale-105 transition-transform cursor-pointer"></LogIn>
-          </div>
-        </div>
-      </div>
-    </>
+    <div>
+      <nav className="flex h-screen justify-between flex-col bg-white py-4 w-20">
+        <ul className="flex flex-col items-center gap-5">
+          <li>
+            {/* svg scales poorly, consider manually adjusting width and height */}
+            <img className="h-10 hover:scale-105 transition-transform cursor-pointer" src="/assets/tempIcon.svg"></img>
+          </li>
+          <hr className="w-9/12 size-0.5 bg-gray-200"/>
+          <li><Handshake className="hover:scale-105 transition-transform cursor-pointer"/></li>
+        </ul>
+
+        <ul className="flex flex-col items-center gap-8">
+          <li><CircleUserRound className="hover:scale-105 transition-transform cursor-pointer"/></li>
+          <li><LogIn className="hover:scale-105 transition-transform cursor-pointer"/></li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
