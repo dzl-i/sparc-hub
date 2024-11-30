728f7d37021245e4b2ff6e0b44292c6ba9e978e5
Sebastien Singarayar <z5444568@ad.unsw.edu.au>
2024-11-30 19:35:14 +1100

    feat: add tag group component for review society modal

---
+++ client/src/components/TagGroup.tsx
@@ -0,0 +1,24 @@
+import Tag from "@/components/Tag";
+
+function TagGroup() {
+  return (
+    <>
+      <div className="relative w-full">
+        <div className="relative">
+          <div
+            className="flex flex-wrap gap-2 bg-[hsl(50,21%,95%)] border-[0.5px] border-gray-400 w-full h-auto p-3 font-spartan leading-tight"
+          >
+            {/* Tags go here!!! */}
+          </div>
+          <div
+            className="bg-[hsl(50,21%,95%)] px-2 text-[12px] absolute left-2 -top-2 font-spartan text-gray-500"
+          >
+            Tags
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TagGroup;
