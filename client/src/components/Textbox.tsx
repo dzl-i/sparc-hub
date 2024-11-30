c67d5d9a61aad01fa604c471c95d88299d1352ec
Sebastien Singarayar <z5444568@ad.unsw.edu.au>
2024-11-30 18:39:18 +1100

    feat: add textbox component for review society modal

---
+++ client/src/components/Textbox.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from "react";
+
+function Textbox({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <div className="relative w-full">
+        <textarea
+          className="bg-[hsl(50,21%,95%)] text-base border-[0.5px] border-gray-400 w-full h-[180px] px-2 py-2 font-spartan leading-tight focus:outline-none focus:border-black resize-none"
+        />
+        <div
+          className="bg-[hsl(50,21%,95%)] px-2 text-[12px] absolute left-2 top-[-7px] font-spartan text-gray-500"
+        >
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Textbox;
