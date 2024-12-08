c2f0d20fd28d3a92a04ced3fb5c672fbe84d0344
dzl-i <denzel.iskandar@gmail.com>
2024-12-08 16:35:25 +0700

    fix: removed review summary

--- client/src/components/ReviewSocietyModal.tsx
+++ client/src/components/ReviewSocietyModal.tsx
@@ -9,16 +9,12 @@ import Image from "next/image";
 interface ReviewSocietyModalProps {
   name: string;
   logo?: string;
-  avgRating?: 'Overwhelmingly Positive' | 'Very Positive' | 'Positive' | 'Mixed' | 'Mostly Negative' | 'Negative';
-  numReviews: number;
   onClose: () => void;
 }
 
 function ReviewSocietyModal({
   name,
   logo = "https://cdn.linkupevents.com/arc_logo.png",
-  avgRating,
-  numReviews,
   onClose
 }: ReviewSocietyModalProps) {
   return (
@@ -29,15 +25,15 @@ function ReviewSocietyModal({
         <div className="flex flex-col pb-6">
           {/* header */}
           <div className="flex flex-col bg-[hsl(30,9%,17%)] p-2">
-            <div className="flex justify-between items-start">
-              <div className="text-[hsl(50,21%,95%)] text-[34px] pl-4">Submit Review</div>
+            <div className="flex justify-between items-center mb-1">
+              <div className="text-[hsl(50,21%,95%)] text-2xl pl-4">Submit Review</div>
               <button onClick={onClose}>
-                <X size="40" strokeWidth="0.5" color="hsl(50,21%,95%)"/>
+                <X size="30" strokeWidth="0.5" color="hsl(50,21%,95%)" />
               </button>
             </div>
             <hr></hr>
             {/* profile div */}
-            <div className="flex pl-5 pt-3 gap-4 pb-2">
+            <div className="flex flex-row items-center pl-5 pt-3 gap-4 pb-2">
               <Image
                 width={70}
                 height={70}
@@ -46,28 +42,19 @@ function ReviewSocietyModal({
                 className="rounded-full"
               />
               <div className="flex flex-col leading-none pt-2">
-                <div className="text-[hsl(50,21%,95%)] text-[30px]">{name}</div>
-                {numReviews > 0 ? (
-                  <div className="self-start p-1 text-[hsl(50,21%,95%)] text-[12px] font-spartan bg-[hsl(85,49%,40%)] rounded-md">
-                    {avgRating} &#40;{numReviews}&#41;
-                  </div>
-                ) : (
-                  <div className="self-start p-1 text-[hsl(50,21%,95%)] text-[12px] font-spartan bg-[hsl(85,49%,40%)] rounded-md">
-                    No reviews yet
-                  </div>
-                )}
+                <div className="text-[hsl(50,21%,95%)] text-4xl">{name}</div>
               </div>
             </div>
           </div>
           {/* content */}
           <div className="flex flex-col items-center justify-center px-16 gap-4 bg-[hsl(50,21%,95%)]">
-            <div className="pt-10 pb-3"><StarRating/></div>
-            <InputField>Title</InputField>
-            <Textbox>Review</Textbox>
-            <TagGroup/>
-            <div className="flex gap-1 self-start pl-1 pt-1">
-              <input type="checkbox" id="anonymous"/>
-              <label className="font-spartan text-sm font-bold">Display anonymously</label>
+            <div className="pt-10 pb-3"><StarRating /></div>
+            <InputField placeholder="Enter review title...">Title</InputField>
+            <Textbox placeholder="Enter review...">Review</Textbox>
+            <TagGroup />
+            <div className="flex items-center gap-2 self-start pl-1 pt-1">
+              <input type="checkbox" id="anonymous" />
+              <label className="font-spartan text-sm font-bold">Display Anonymously</label>
             </div>
             <div className="self-end">
               <Button onClick={onClose}>
