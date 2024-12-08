ab90316297c72e441933e02b3079b3427040f139
dzl-i <denzel.iskandar@gmail.com>
2024-12-08 16:23:31 +0700

    feat: add placeholder values

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,12 +1,13 @@
 import React, { ReactNode } from "react";
 
-function InputField({ children }: { children: ReactNode }) {
+function InputField({ children, placeholder = "" }: { children: ReactNode, placeholder: string }) {
   return (
     <>
       <div className="relative w-full">
         <input
-          className="bg-[hsl(50,21%,95%)] text-base border-[0.5px] border-gray-400 w-full h-10 px-2 font-spartan leading-tight focus:outline-none focus:border-black"
+          className="bg-[hsl(50,21%,95%)] text-base border-[0.5px] border-gray-400 w-full h-10 px-2 font-spartan leading-tight focus:outline-none focus:border-black placeholder:text-gray-400 placeholder:text-sm"
           type="text"
+          placeholder={placeholder}
         />
         <div
           className="bg-[hsl(50,21%,95%)] px-2 text-[12px] absolute left-2 bottom-7 font-spartan text-gray-500"
--- client/src/components/Textbox.tsx
+++ client/src/components/Textbox.tsx
@@ -1,11 +1,12 @@
 import React, { ReactNode } from "react";
 
-function Textbox({ children }: { children: ReactNode }) {
+function Textbox({ children, placeholder }: { children: ReactNode, placeholder: string }) {
   return (
     <>
       <div className="relative w-full">
         <textarea
-          className="bg-[hsl(50,21%,95%)] text-base border-[0.5px] border-gray-400 w-full h-auto min-h-[120px] px-2 py-2 font-spartan leading-tight focus:outline-none focus:border-black resize-none"
+          className="bg-[hsl(50,21%,95%)] text-base border-[0.5px] border-gray-400 w-full h-auto min-h-[120px] px-2 py-2 font-spartan leading-tight focus:outline-none focus:border-black resize-none placeholder:text-gray-400 placeholder:text-sm"
+          placeholder={placeholder}
         />
         <div
           className="bg-[hsl(50,21%,95%)] px-2 text-[12px] absolute left-2 top-[-7px] font-spartan text-gray-500"
