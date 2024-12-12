2abb2d096e1ec5930d59a3877c1d1c0580246b4e
Elvene <123799877+xleonx0x@users.noreply.github.com>
2024-12-13 02:35:36 +1100

    feat: added login and register page

--- client/src/app/(noNavbar)/register/page.tsx
+++ client/src/app/(noNavbar)/register/page.tsx
@@ -3,9 +3,10 @@ import { useRouter } from "next/navigation";
 import { createRipple } from "@/components/Button";
 import { useEffect, useState } from "react";
 
-export default function LoginPage() {
+export default function RegisterPage() {
   const [zid, setZid] = useState("");
   const [zpass, setZpass] = useState("");
+  const [confirmZpass, setConfirmZpass] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
 
   const zidChecker = /^z\d{7}$/;
@@ -24,7 +25,7 @@ export default function LoginPage() {
 
   useEffect(() => {
     setErrorMsg("");
-  }, [zid, zpass]);
+  }, [zid, zpass, confirmZpass]);
 
   const submit = (zid: string, zpass: string) => {
     let error = "";
@@ -32,8 +33,14 @@ export default function LoginPage() {
       error = "Zid cannot be empty";
     } else if (zpass === "") {
       error = "zpass cannot be empty";
+    } else if (confirmZpass === "") {
+      error = "confirm password cannot be empty";
+    } else if (zpass != confirmZpass) {
+      error = "passwords do not match";
     } else if (!zid.match(zidChecker)) {
       error = "Zid incorrect format!";
+    } else if (zpass.length < 10) {
+      error = "zpass cannot be below 10 character";
     }
 
     setErrorMsg(error);
@@ -52,7 +59,7 @@ export default function LoginPage() {
       <div className="flex h-screen w-screen justify-center items-center">
         <div className="w-1/5 bg-[#b7e6a5b0] px-8 pt-6 pb-4 rounded-lg">
           <h1 className="text-center font-lalezar text-textGreen text-4xl mb-2">
-            Welcome to SparcHub
+            Register
           </h1>
           <form className="font-spartan">
             <div className="flex flex-col gap-4">
@@ -88,6 +95,22 @@ export default function LoginPage() {
                   onChange={(e) => setZpass(e.target.value)}
                 />
               </div>
+              <div>
+                <label
+                  className="block text-lg font-spartan font-bold mb-2"
+                  htmlFor="zpass"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  className="w-full py-2 px-2 rounded bg-slate-100 border-black border leading-tight focus:outline-none focus:shadow-outline"
+                  id="zpass"
+                  placeholder="zpass"
+                  type="password"
+                  value={confirmZpass}
+                  onChange={(e) => setConfirmZpass(e.target.value)}
+                />
+              </div>
               {errorMsg && <p className="text-md text-red-800">{errorMsg}</p>}
               <button
                 type="button"
@@ -97,24 +120,15 @@ export default function LoginPage() {
                   submit(zid, zpass);
                 }}
               >
-                Login
+                Register
               </button>
-              <div className="flex flex-col gap-1">
-                <button
-                  type="button"
-                  className="text-md font-semibold"
-                  onClick={() => router.push("/register")}
-                >
-                  No account? Register here
-                </button>
-                <button
-                  type="button"
-                  className="text-md font-semibold"
-                  onClick={handleGoBack}
-                >
-                  Go back
-                </button>
-              </div>
+              <button
+                type="button"
+                className="text-md font-semibold"
+                onClick={handleGoBack}
+              >
+                Go back
+              </button>
             </div>
           </form>
         </div>
